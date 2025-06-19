f31c2a772a5f5863ff04f8600669ede3f59cbf29
abdulbasit92786 <hassenahassena711@gmail.com>
2025-06-19 16:55:58 +0500

    Create taskData.js

---
+++ taskData.js
@@ -0,0 +1,18 @@
+// File: taskData.js
+const tasks = [
+  {
+    title: "🟢 Sign up on TimeBucks",
+    url: "https://www.timebucks.com/?refID=123456",
+    reward: "2 DOGE"
+  },
+  {
+    title: "🔵 Complete Survey on CPALead",
+    url: "https://www.cpalead.com/task456",
+    reward: "3 DOGE"
+  },
+  {
+    title: "🟡 Watch Ad and Subscribe",
+    url: "https://www.youtube.com/watch?v=abc123",
+    reward: "1 DOGE"
+  }
+];
