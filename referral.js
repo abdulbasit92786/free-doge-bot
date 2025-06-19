0cb2c61bd5469f49dbbb002c1629b73df441f70d
abdulbasit92786 <hassenahassena711@gmail.com>
2025-06-19 17:40:57 +0500

    Update referral.js

--- referral.js
+++ referral.js
@@ -1,6 +1,6 @@
 // File: referral.js
 function getReferral() {
-  const userId = "USER123"; // Replace with dynamic user ID
+  const userId = "7584161210"; // Replace with dynamic user ID
   const referralLink = `${window.location.origin}/?ref=${userId}`;
   document.getElementById("referralLink").innerText = `Your referral link: ${referralLink}`;
 }
