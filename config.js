88ffef58b8d49578530a77c3693f3dd83c07a0ba
abdulbasit92786 <hassenahassena711@gmail.com>
2025-06-19 17:31:43 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+
+// Replace this with your actual Telegram Bot Token
+const BOT_TOKEN = "YOUR_BOT_TOKEN_HERE";
+
+// Replace this with your Telegram Numeric Admin ID
+const ADMIN_ID = 123456789;
+
+export { BOT_TOKEN, ADMIN_ID };
