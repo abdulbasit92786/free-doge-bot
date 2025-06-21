4cf6ab0d2e3b8133642f085aadada1238dafc981
abdulbasit92786 <hassenahassena711@gmail.com>
2025-06-21 10:53:46 +0500

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-
-// Replace this with your actual Telegram Bot Token
-const BOT_TOKEN = "7436258467:AAHOf0R4lMn21XrOrJGIt-L5IuYu3yR9phs";
-
-// Replace this with your Telegram Numeric Admin ID
-const ADMIN_ID = 7584161210;
-
-export { 7436258467:AAHOf0R4lMn21XrOrJGIt-L5IuYu3yR9phs, 7584161210 };
