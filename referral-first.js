2f785ee6a9eec2180fdc39bed3302e676984f018
abdulbasit92786 <hassenahassena711@gmail.com>
2025-06-21 10:53:00 +0500

    Delete referral-first.js

--- referral-first.js
+++
@@ -1,6 +0,0 @@
-// File: referral.js
-function getReferral() {
-  const userId = "7584161210"; // Replace with dynamic user ID
-  const referralLink = `${window.location.origin}/?ref=${userId}`;
-  document.getElementById("referralLink").innerText = `Your referral link: ${referralLink}`;
-}
