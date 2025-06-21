94e8e77fc5b34f5243e2152515c238e50cf34c03
abdulbasit92786 <hassenahassena711@gmail.com>
2025-06-21 10:49:49 +0500

    Delete withdraw.js

--- withdraw.js
+++
@@ -1,8 +0,0 @@
-// File: withdraw.js
-document.getElementById("withdrawForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-  const wallet = document.getElementById("wallet").value;
-  const amount = document.getElementById("amount").value;
-
-  document.getElementById("withdrawMessage").innerText = `✅ Withdraw request of ${amount} DOGE sent to ${wallet}`;
-});
